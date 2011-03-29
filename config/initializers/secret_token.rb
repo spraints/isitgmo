df3ff744ea5cbd884010082ff951543b3da7fc55
Matt Burke <spraints@gmail.com>
2011-03-28 22:10:04 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Isitgmo::Application.config.secret_token = 'bcfa9e2ad887403037dfd3c8e0b8ebca5f939817826e8011c6a83377309cd4bbcf912cc3c14701c3996ceffe3db791cb9738fb73f6b8889a680730f971402656'
