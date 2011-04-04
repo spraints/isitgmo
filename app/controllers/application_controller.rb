6d283f82d5ea28e525e9f450b889f803380af0b7
Matt Burke <spraints@gmail.com>
2011-04-04 10:02:49 -0400

    Add cache-control header for the 'static' home page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+  def cache_for span = 5.minutes
+    response.headers['Cache-Control'] = "public, max-age=#{span}"
+  end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    cache_for 1.day
   end
-
 end
